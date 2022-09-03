32ba386b3f244fcd31b5f845b97686c58dab0ab0
ADEEL HAIDER <adeelhaider1212@gmail.com>
2022-09-03 13:04:01 +0500

    GETX
    ChangeTheme || change Theme

---
+++ Getxapp/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp"
+  "FLUTTER_TARGET=D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\FlutterPractices\\Flutter_Devnation\\Flutter_Devnation\\Getxapp\\.dart_tool\\package_config.json"
+)
